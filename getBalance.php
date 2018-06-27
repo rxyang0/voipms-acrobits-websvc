dc8eeaf7e5cc4b4cbb3f5a22753fc30af7f2e2d3
Richard Yang <yangrichard@protonmail.com>
2018-06-27 15:49:22 -0600

    Update getBalance.php

--- getBalance.php
+++ getBalance.php
@@ -1,28 +1,22 @@
 <?
 
-if (isset($_GET['api_username']) && isset($_GET['api_password'])) {
-    
-    $postfields = array(
-        'api_username'=>$_GET['api_username'],
-        'api_password'=>$_GET['api_password'],
-        'method'=>'getBalance'
-    );
+$postfields = array(
+    'api_username'=>$_GET['api_username'],
+    'api_password'=>$_GET['api_password'],
+    'method'=>'getBalance'
+);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt($ch, CURLOPT_POST, true );
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-    curl_setopt($ch, CURLOPT_URL, "https://voip.ms/api/v1/rest.php");
-    $result = curl_exec($ch);
-    curl_close($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+curl_setopt($ch, CURLOPT_URL, "https://voip.ms/api/v1/rest.php");
+$result = curl_exec($ch);
+curl_close($ch);
 
-    $data=json_decode($result,true);
-    echo $data;
+$data = json_decode($result, true);
+echo $data->{'current_balance'};
 
-//    header('Content-Type: application/json; charset=utf-8');
-//    header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-//    echo json_encode($data);
-
-} else {
-    echo "No username/password provided.";
-}
+// header('Content-Type: application/json; charset=utf-8');
+// header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+// echo json_encode($data);
