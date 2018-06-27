cbcf84ce69fa2f8d531534404f327419ef5a0f59
Richard Yang <yangrichard@protonmail.com>
2018-06-27 17:07:49 -0600

    Update getBalance.php

--- getBalance.php
+++ getBalance.php
@@ -10,16 +10,16 @@ $postfields = array(
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-curl_setopt($ch, CURLOPT_URL, "https://voip.ms/api/v1/rest.php");
+curl_setopt($ch, CURLOPT_URL,
+    "https://voip.ms/api/v1/rest.php?api_username=$_GET['api_username']&api_password=$_GET['api_password']&method=getBalance"
+);
 $data = json_decode(curl_exec($ch), true);
 curl_close($ch);
 
-if ($data['status'] != "success") {
+if ($data['status'] == "success") {
     $result = '{"balanceString" : "' . $currency . ' ' . round($data['balance']['current_balance'], 2) . '"}';
 } else {
-    $result = '{"balanceString" : "BAL ERROR"}';
+    $result = '{"balanceString" : "BAL CHK ERROR"}';
 }
 
 header('Content-Type: application/json; charset=utf-8');
