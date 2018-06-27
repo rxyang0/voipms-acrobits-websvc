e6e7749c3f2b6ceb127fd7bdf9cb310eb52fe3c9
Richard Yang <yangrichard@protonmail.com>
2018-06-27 12:27:20 -0600

    Create voip-ms-websvc.php

---
+++ voip-ms-websvc.php
@@ -0,0 +1,20 @@
+<?
+$postfields = array(
+    'api_username'=>'yangrichard@protonmail.com',
+    'api_password'=>'&v$PW?2o!s3#dGN*',
+    'method'=>'getBalance'
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_POST, true );
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+curl_setopt($ch, CURLOPT_URL, "https://voip.ms/api/v1/rest.php");
+$result = curl_exec($ch);
+curl_close($ch);
+
+$data=json_decode($result,true);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
