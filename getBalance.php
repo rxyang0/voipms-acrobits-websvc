8c7ef9b301e7343b64a5fa8671f93479d217c292
Richard Yang <yangrichard@protonmail.com>
2018-06-27 17:12:08 -0600

    Update getBalance.php

--- getBalance.php
+++ getBalance.php
@@ -1,7 +1,5 @@
 <?
-
 $currency = "USD";
-
 $postfields = array(
     'api_username'=>$_GET['api_username'],
     'api_password'=>$_GET['api_password'],
@@ -10,16 +8,16 @@ $postfields = array(
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,
-    "https://voip.ms/api/v1/rest.php?api_username=$_GET['api_username']&api_password=$_GET['api_password']&method=getBalance"
-);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+curl_setopt($ch, CURLOPT_URL, "https://voip.ms/api/v1/rest.php");
 $data = json_decode(curl_exec($ch), true);
 curl_close($ch);
 
-if ($data['status'] == "success") {
+if ($data['status'] != "success") {
     $result = '{"balanceString" : "' . $currency . ' ' . round($data['balance']['current_balance'], 2) . '"}';
 } else {
-    $result = '{"balanceString" : "BAL CHK ERROR"}';
+    $result = '{"balanceString" : "BAL ERROR"}';
 }
 
 header('Content-Type: application/json; charset=utf-8');
